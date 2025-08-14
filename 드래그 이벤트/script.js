040d404899740ed0e22ed8f39d9f777e350b6def
윤태웅 <24457545yong@gmail.com>
2025-08-14 18:28:21 +0900

    Add script.js for 드래그 이벤트

---
+++ 드래그 이벤트/script.js
@@ -0,0 +1,14 @@
+// 간단하게 구현하는 이미지 이동
+function dragEnter(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
